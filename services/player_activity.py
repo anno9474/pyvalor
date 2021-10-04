e9490df6171bb8bfc1d81a5c5facaeaa985daea9
classAndrew <andrxwlin@gmail.com>
2021-10-04 16:17:38 -0700

    add datetime before heartbeat

--- services/player_activity.py
+++ services/player_activity.py
@@ -4,12 +4,15 @@ from db import Connection
 from configs import guilds
 from network import Async
 import time
+import datetime
 import sys
 
 SLEEP = 3600
 
 async def player_activity_task():
     while True:
+        start = time.time()
+
         URL = "https://api.wynncraft.com/public_api.php?action=guildStats&command="
         online_all = await Async.get("https://api.wynncraft.com/public_api.php?action=onlinePlayers")
         online_all = {y for x in online_all for y in online_all[x] if not "request" in x}
@@ -22,5 +25,8 @@ async def player_activity_task():
                     inserts.append(f"(\"{name}\", \"{guild}\", {int(time.time())}, \"{members[name]}\")")
 
         Connection.execute(f"INSERT INTO activity_members VALUES {','.join(inserts)}")
+
+        end = time.time()
+        print(datetime.datetime.now().ctime(), "PLAYER ACTIVITY TASK", end-start, "s")
         
         await asyncio.sleep(SLEEP)
