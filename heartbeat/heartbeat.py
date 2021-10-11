37822c12a09211396d174955168232a170d02b3a
classAndrew <andrxwlin@gmail.com>
2021-10-11 12:27:26 -0700

    .

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -8,10 +8,10 @@ import asyncio
 class Heartbeat:
     wsconns = {}
     tasks: Task = [
-        # TerritoryTrackTask(60, wsconns),
+        TerritoryTrackTask(60, wsconns),
         PlayerActivityTask(3600),
-        # GXPTrackerTask(1800),
-        # GuildActivityTask(300)
+        GXPTrackerTask(1800),
+        GuildActivityTask(300)
     ]
     
     @staticmethod
--- heartbeat/task.py
+++ heartbeat/task.py
@@ -27,7 +27,6 @@ class Task:
         while not self.finished:
             task = asyncio.get_event_loop().create_task(coro())
             task.add_done_callback(self.done_callback)
-
+            await asyncio.wait_for(task)
             print(datetime.datetime.now().ctime(), "Restarting (Error Occured?)")
-
             await asyncio.sleep(10)
