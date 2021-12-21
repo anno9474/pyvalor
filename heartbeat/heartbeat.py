10854fe63c7fc34fc69b3faec77f1c132f44165c
classAndrew <classAndrew@users.noreply.github.com>
2021-12-21 10:31:44 -0800

    add player stats

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -3,6 +3,7 @@ from .terr_tracker import TerritoryTrackTask
 from .player_activity import PlayerActivityTask
 from .gxp_tracker import GXPTrackerTask
 from .guild_activity import GuildActivityTask
+from .player_stats import PlayerStatsTask
 import asyncio
 
 class Heartbeat:
@@ -11,7 +12,8 @@ class Heartbeat:
         TerritoryTrackTask(60, wsconns),
         PlayerActivityTask(3600),
         GXPTrackerTask(1800),
-        GuildActivityTask(300)
+        GuildActivityTask(300),
+        PlayerStatsTask(3600)
     ]
     
     @staticmethod
---
+++ heartbeat/player_stats.py
@@ -0,0 +1,90 @@
+import asyncio
+import aiohttp
+from db import Connection
+from configs import guilds
+from network import Async
+from .task import Task
+import time
+import datetime
+import sys
+from dotenv import load_dotenv
+import json
+import os
+
+load_dotenv()
+api_key = os.environ["API_KEY"]
+
+class PlayerStatsTask(Task):
+    def __init__(self, sleep):
+        super().__init__(sleep)
+        
+    def stop(self):
+        self.finished = True
+        self.continuous_task.cancel()
+
+    def run(self):
+        self.finished = False
+        idx = {'uuid': 0, 'firstjoin': 1, 'Decrepit Sewers': 2, 'Infested Pit': 3, 'Lost Sanctuary': 4, 'Underworld Crypt': 5, 'Sand-Swept Tomb': 6, 'Ice Barrows': 7, 'Undergrowth Ruins': 8, "Galleon's Graveyard": 9, 'Fallen Factory': 10, 'Eldritch Outlook': 11,'Corrupted Decrepit Sewers': 12, 'Corrupted Infested Pit': 13, 'Corrupted Lost Sanctuary': 14, 'Corrupted Underworld Crypt': 15, 'Corrupted Sand-Swept Tomb': 16, 'Corrupted Ice Barrows': 17, 'Corrupted Undergrowth Ruins,': 18, 'itemsIdentified': 19, 'chestsFound': 20, 'blocksWalked': 21, 'logins': 22, 'playtime': 23, 'alchemism': 24, 'armouring': 25, 'combat': 26, 'cooking': 27, 'farming': 28, 'fishing': 29, 'jeweling': 30, 'mining': 31, 'scribing': 32, 'tailoring': 33, 'weaponsmithing': 34, 'woodcutting': 35, 'woodworking': 36, 'Nest of the Grootslangs': 37, 'The Canyon Colossus': 38, "mobsKilled": 39, "deaths": 40}
+        async def player_stats_task():
+            while not self.finished:
+                print(datetime.datetime.now().ctime(), "PLAYER STATS TRACK START")
+                start = time.time()
+
+                online_all = await Async.get("https://api.wynncraft.com/public_api.php?action=onlinePlayers")
+                online_all = {y for x in online_all for y in online_all[x] if not "request" in x}
+
+                inserts = []
+                uuid_name = []
+                cnt = 0
+
+                for player in online_all:
+                    uri = f"https://api.wynncraft.com/v2/player/{player}/stats?apikey="+api_key
+                    stats = (await Async.get(uri))["data"][0]
+                    row = [0]*len(idx)
+                    uuid = stats["uuid"]
+                    row[idx["uuid"]] = uuid
+                    row[idx["firstjoin"]] = datetime.datetime.fromisoformat(stats["meta"]["firstJoin"][:-1]).timestamp()
+
+                    for cl in stats["classes"]:
+                        for dung in cl.get("dungeons", {"list": []})["list"]:
+                            if dung["name"] in idx:
+                                row[idx[dung["name"]]] += dung["completed"]
+
+                        for raid in cl.get("raids", {"list": []})["list"]:
+                            if raid["name"] in idx:
+                                row[idx[raid["name"]]] += raid["completed"]
+
+                        row[idx["itemsIdentified"]] += cl["itemsIdentified"]
+                        row[idx["mobsKilled"]] += cl["mobsKilled"]
+                        row[idx["chestsFound"]] += cl["chestsFound"]
+                        row[idx["blocksWalked"]] += cl["blocksWalked"]
+                        row[idx["logins"]] += cl["logins"]
+                        row[idx["deaths"]] += cl["deaths"]
+                        row[idx["playtime"]] += cl["playtime"]
+                        
+                        for prof in cl["professions"]:
+                            xp = cl["professions"][prof]["xp"]
+                            row[idx[prof]] += cl["professions"][prof]["level"] + (xp if xp else 0)/100
+
+                    inserts.append(row)
+                    uuid_name.append((uuid, player))
+                    cnt += 1
+
+                    if inserts and (cnt % 50 == 0 or cnt == len(online_all)-1):
+                        query_stats = "REPLACE INTO player_stats VALUES " + ','.join(f"('{x[0]}', {str(x[1])}, {','.join(map(str, x[2:]))})" for x in inserts)
+                        query_uuid = "REPLACE INTO uuid_name VALUES " + ','.join(f"(\'{uuid}\',\'{name}\')" for uuid, name in uuid_name)
+                        Connection.execute(query_stats)
+                        Connection.execute(query_uuid)
+                        inserts = []
+                        uuid_name = []
+
+                    await asyncio.sleep(0.3)
+
+                end = time.time()
+                print(datetime.datetime.now().ctime(), "PLAYER STATS TASK", end-start, "s")
+                
+                await asyncio.sleep(self.sleep)
+        
+            print(datetime.datetime.now().ctime(), "PlayerStatsTask finished")
+
+        self.continuous_task = asyncio.get_event_loop().create_task(self.continuously(player_stats_task))
